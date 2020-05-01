25dbf038d449240d6c25b39e453a72411cdea997
akter_rakib <rakibccj@gmail.com>
2020-05-01 16:39:03 +0600

    inject dependency by constructor

--- omod/src/main/java/org/openmrs/module/metaapps/web/controller/HomeController.java
+++ omod/src/main/java/org/openmrs/module/metaapps/web/controller/HomeController.java
@@ -7,21 +7,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.openmrs.module.webservices.rest.web.RestConstants;
 
 @Controller
 public class HomeController {
 	
-	@Autowired
 	CoreDeveloperService coreDeveloperService;
 	
+	public HomeController() {
+		
+	}
+	
+	@Autowired
+	public HomeController(CoreDeveloperService coreDeveloperService) {
+		this.coreDeveloperService = coreDeveloperService;
+	}
+	
 	@ResponseBody
-	@RequestMapping(value = "module/metaapps/greeting", method = RequestMethod.GET)
+	@RequestMapping(value = "/module/metaapps/greeting", method = RequestMethod.GET)
 	public String greeting() {
 		return "Hello OpenMRS+ Developer";
 	}
 	
 	@ResponseBody
-	@RequestMapping(value = "module/metaapps/saveDev", method = RequestMethod.GET)
+	@RequestMapping(value = "/module/metaapps/saveDev", method = RequestMethod.GET)
 	public String saveDeveloper() {
 		CoreDeveloper coreDeveloper = new CoreDeveloper("Rakib", 1, "rakibccj@gmail.com");
 		coreDeveloperService.saveDev(coreDeveloper);
